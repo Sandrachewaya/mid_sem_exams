228b04e7be6d607a5bb726921fffa5e7a846acfb
sandra <sandrachewaya@gmail.com>
2025-08-06 08:14:03 +0200

    question3 commit

---
+++ question3.js
@@ -0,0 +1,10 @@
+let a = 15;
+let b = 5;
+
+console.log("division:", a/b);
+console.log("logical NOT:", a !== b);
+console.log("Ternary:", a > b ? "A is greater" : "B is greater");
+a++;
+b--;
+console.log("Increment a:", a);
+console.log("Decrement b:", b);
