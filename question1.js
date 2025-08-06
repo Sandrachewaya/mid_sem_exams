c32707ebd718cee5075a2f35b077d80163d72518
sandra <sandrachewaya@gmail.com>
2025-08-06 07:58:51 +0200

    intial commit

---
+++ question1.js
@@ -0,0 +1,14 @@
+
+let name = "sandra";
+let age = 20;
+let isStudent = true;
+let emptyValue = null; 
+let person = { firstName: "Peace", lastName: "chewaya" };
+let cars = ["BMW", "Ford", "CX-5"]; 
+
+console.log("String:", name);
+console.log("Number:", age);
+console.log("Boolean:", isStudent);
+console.log("Null:", emptyValue);
+console.log("Object:", person);
+console.log("Array:", cars);
